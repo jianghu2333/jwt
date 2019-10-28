8a56f2dae68433487c2cfefa28e66294279d8b1c
jianghu <jianghu2333>
2019-10-28 21:02:49 +0800

    添加拦截器

--- src/main/java/com/jianghu/jwt/filter/LoginFilter.java
+++ src/main/java/com/jianghu/jwt/filter/LoginFilter.java
@@ -1,10 +1,20 @@
 package com.jianghu.jwt.filter;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @Auther: JY
  * @Date: 2019/10/28 16:07
  * @Description:
  */
-public class LoginFilter {
+public abstract class LoginFilter implements HandlerInterceptor {
 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.getAttribute("");
+        return false;
+    }
 }
